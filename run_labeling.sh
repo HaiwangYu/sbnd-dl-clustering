7d4a597e2426522716f49a21dc40db9a79421d71
HaiwangYu <yuhw.pku@gmail.com>
2025-06-11 14:41:17 -0500

    cleanup

--- run_labeling.sh
+++
@@ -1,1 +0,0 @@
-python labeling.py --tru-prefix tru-apa1 --rec-prefix rec-apa1 --out-prefix rec-lab-apa1 --entries 0-1
--- zip-upload.sh
+++
@@ -1,5 +0,0 @@
-rm -f upload.zip
-cd bee
-zip -r ../upload data
-cd -
-./upload-to-bee.sh upload.zip
